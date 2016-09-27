dff8286fcbaa87e6ea367a2c8f18e14b85deeeee
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2016-09-27 03:58:06 +0000

    DEVER04

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     
   resources :infos, only: [:index, :new, :create]
   resources :boards, only: [:index, :new, :create]
+  
+  root 'top#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
