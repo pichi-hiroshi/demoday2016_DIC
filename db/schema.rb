afda3cbc8e7e754085005922c373b69a4a7514f9
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2016-10-02 11:30:41 +0000

    mail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927085924) do
+ActiveRecord::Schema.define(version: 20161002112205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20160927085924) do
     t.text     "datasource"
     t.text     "datasource_url"
     t.integer  "user_id"
+    t.string   "username"
+    t.string   "provider"
+    t.string   "image_url"
+    t.string   "avatar"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
@@ -57,9 +61,19 @@ ActiveRecord::Schema.define(version: 20160927085924) do
     t.datetime "updated_at",                             null: false
     t.string   "name"
     t.boolean  "admin",                  default: false
+    t.string   "uid",                    default: "",    null: false
+    t.string   "provider",               default: "",    null: false
+    t.string   "image_url"
+    t.string   "avatar"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
