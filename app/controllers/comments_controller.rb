a0ee88b8f887948c871f104e851944773a518b71
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2016-10-04 10:26:23 +0000

    フォローなど

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,6 +35,7 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
   
+  
  private
     def comment_params
       params.require(:comment).permit(:post_id, :content)
